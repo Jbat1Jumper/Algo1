faef0e4d804f9992283e90d26a89be25e45ca454
leamotta <leamotta@msn.com>
2014-10-24 20:55:07 -0300

    Constructor y Obtener Pixel

---
+++ TP2/imagen.cpp
@@ -0,0 +1,27 @@
+#include "imagen.h"
+
+Imagen::Imagen(int alto_param,int ancho_param)
+{
+    int i=0;
+    int j=0;
+    Pixel pixelNegro = Pixel(0,0,0);
+    Pixel1DContainer fila = Pixel1DContainer();
+    Pixel2DContainer img = Pixel2DContainer();
+    while(j<ancho_param)
+            {
+                fila.push_back(pixelNegro);
+                j++;
+            }
+
+    while (i<alto_param)
+        {
+            img.push_back(fila);
+            i++;
+        }
+    pixels = img;
+}
+
+Pixel Imagen::obtenerPixel(int fila, int columna) const
+{
+    return (pixels.at(fila)).at(columna);
+}
